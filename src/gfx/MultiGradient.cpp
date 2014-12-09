8f1386132ac1342738813322121a927e32733841
Ryuichiro Ishitani <ryu.istn@gmail.com>
2014-12-09 22:36:43 +0900

    added multi-color gradient class

---
+++ src/gfx/MultiGradient.cpp
@@ -0,0 +1,141 @@
+// -*-Mode: C++;-*-
+//
+//  Multi-color gradient class
+//
+
+#include <common.h>
+
+#include "MultiGradient.hpp"
+#include <qlib/Utils.hpp>
+#include <qlib/Vector4D.hpp>
+
+using namespace gfx;
+
+MC_CLONEABLE_IMPL(gfx::MultiGradient);
+
+MultiGradient::MultiGradient()
+{
+}
+
+/// get color
+ColorPtr MultiGradient::getColor(double rho) const
+{
+  if (m_data.empty())
+    return gfx::SolidColor::createRGB(0.0, 0.0, 0.0, 1.0);
+
+  data_t::const_iterator iter = m_data.begin();
+
+  if (m_data.size()==1)
+    return iter->pColor;
+
+  data_t::const_iterator eiter = m_data.end();
+  data_t::const_iterator iter2 = eiter;
+  --iter2;
+  if (iter2->value<=rho) {
+    return iter2->pColor;
+  }
+
+  for (; iter!=eiter; ++iter) {
+    iter2 = iter;
+    iter2++;
+
+    double v1 = iter->value;
+    double v2 = iter2->value;
+    if (v1<=rho && rho<v2) {
+      double rho2 = (rho-v1)/(v2-v1);
+      return ColorPtr(new GradientColor(iter->pColor, iter2->pColor, rho2)); 
+    }
+  }
+
+  // should not be reached here!!
+  MB_ASSERT(false);
+  LOG_DPRINTLN("ERROR!!");
+  return gfx::SolidColor::createRGB(0.0, 0.0, 0.0, 1.0);
+}
+
+MultiGradient::data_t::const_iterator MultiGradient::getIterAt(int ind) const
+{
+  data_t::const_iterator iter = m_data.begin();
+  data_t::const_iterator eiter = m_data.end();
+
+  if (ind=>m_data.size())
+    return eiter;
+
+  int i=ind;
+  while ( i>0 && iter!=eiter) {
+    --i;
+    ++iter;    
+  }
+
+  return iter;
+}
+
+MultiGradient::data_t::iterator MultiGradient::getIterAt(int ind)
+{
+  data_t::iterator iter = m_data.begin();
+  data_t::iterator eiter = m_data.end();
+
+  if (ind=>m_data.size())
+    return eiter;
+
+  int i=ind;
+  while ( i>0 && iter!=eiter) {
+    --i;
+    ++iter;    
+  }
+
+  return iter;
+}
+
+double MultiGradient::getValueAt(int ind) const
+{
+  data_t::const_iterator iter = getIterAt(ind);
+
+  if (iter==m_data.end()) {
+    MB_THROW(qlib::RuntimeException, "error");
+  }
+
+  return iter->value;
+}
+
+ColorPtr MultiGradient::getColorAt(int ind) const
+{
+  data_t::const_iterator iter = getIterAt(ind);
+
+  if (iter==m_data.end()) {
+    MB_THROW(qlib::RuntimeException, "error");
+  }
+
+  return iter->pColor;
+}
+
+bool MultiGradient::removeAt(int ind)
+{
+  data_t::iterator iter = getIterAt(ind);
+
+  if (iter==m_data.end())
+    return false;
+
+  m_data.erase(iter);
+  return true;
+}
+
+bool MultiGradient::changeAt(int ind, double value, const ColorPtr &color)
+{
+  data_t::const_iterator iter = getIterAt(ind);
+
+  if (iter==m_data.end())
+    return false;
+
+  iter->value = value;
+  iter->pColor = color;
+}
+
+void MultiGradient::writeTo2(qlib::LDom2Node *pNode) const
+{
+}
+
+void MultiGradient::readFrom2(qlib::LDom2Node *pNode)
+{
+}
+
