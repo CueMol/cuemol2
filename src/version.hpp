954e3eed6954f6ea58b177b0adec423f45dfdba4
Ryuichiro Ishitani <you@example.com>
2014-12-21 10:21:47 +0900

    release candidate of 2.2.1

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,1,329
-#define PRODUCTVER    2,2,1,329
-#define STRFILEVER    "2.2.1.329"
-#define STRPRODUCTVER "2.2.1.329"
-#define BUILD_ID 20141209181745
-#define STRBUILD_ID "20141209181745"
+#define FILEVER       2,2,1,330
+#define PRODUCTVER    2,2,1,330
+#define STRFILEVER    "2.2.1.330"
+#define STRPRODUCTVER "2.2.1.330"
+#define BUILD_ID 20141221102116
+#define STRBUILD_ID "20141221102116"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
