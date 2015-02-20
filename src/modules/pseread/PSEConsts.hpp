87f6ce40c2faf6682c381597ef87ddf299538a9f
Ryuichiro Ishitani <you@example.com>
2015-02-20 17:28:21 +0900

    _

---
+++ src/modules/pseread/PSEConsts.hpp
@@ -0,0 +1,24 @@
+#ifndef PSE_CONSTANTS_HPP
+#define PSE_CONSTANTS_HPP
+
+// layer3/Executive.c
+static const int ExecObject = 0;
+static const int ExecSelection = 1;
+
+// layer1/PyMOLObject.h
+static const int ObjectMolecule = 1;
+static const int ObjectMap = 2;
+static const int ObjectMesh = 3;
+static const int ObjectMeasurement = 4;
+static const int ObjectCallback = 5;
+static const int ObjectCGO = 6;
+static const int ObjectSurface = 7;
+static const int ObjectGadget = 8;
+static const int ObjectCalculator = 9;
+static const int ObjectSlice = 10;
+static const int ObjectAlignment = 11;
+static const int ObjectGroup = 12;
+static const int ObjectVolume = 13;
+
+#endif
+
