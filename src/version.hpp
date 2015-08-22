f746671bc22721f804cb78cb3c555fed76cc6593
Your Name <you@example.com>
2015-08-22 17:58:42 +0900

    cartoon renderer ribbonhelix impl

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,356
-#define PRODUCTVER    2,2,2,356
-#define STRFILEVER    "2.2.2.356"
-#define STRPRODUCTVER "2.2.2.356"
-#define BUILD_ID 20150725210312
-#define STRBUILD_ID "20150725210312"
+#define FILEVER       2,2,2,357
+#define PRODUCTVER    2,2,2,357
+#define STRFILEVER    "2.2.2.357"
+#define STRPRODUCTVER "2.2.2.357"
+#define BUILD_ID 20150822175832
+#define STRBUILD_ID "20150822175832"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
