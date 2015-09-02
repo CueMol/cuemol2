0d8b434bb78173d125713cff30dde59e066b8fe4
Ryuichiro Ishitani <you@example.com>
2015-09-02 15:22:40 +0900

    _

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,358
-#define PRODUCTVER    2,2,2,358
-#define STRFILEVER    "2.2.2.358"
-#define STRPRODUCTVER "2.2.2.358"
-#define BUILD_ID 20150824172427
-#define STRBUILD_ID "20150824172427"
+#define FILEVER       2,2,2,359
+#define PRODUCTVER    2,2,2,359
+#define STRFILEVER    "2.2.2.359"
+#define STRPRODUCTVER "2.2.2.359"
+#define BUILD_ID 20150902151020
+#define STRBUILD_ID "20150902151020"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
