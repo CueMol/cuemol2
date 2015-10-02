70454b0874425b62862a6ac0f868a834c1faf01e
Ryuichiro Ishitani <you@example.com>
2015-10-02 17:38:58 +0900

    winxp support

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,365
-#define PRODUCTVER    2,2,2,365
-#define STRFILEVER    "2.2.2.365"
-#define STRPRODUCTVER "2.2.2.365"
-#define BUILD_ID 20150924090858
-#define STRBUILD_ID "20150924090858"
+#define FILEVER       2,2,2,366
+#define PRODUCTVER    2,2,2,366
+#define STRFILEVER    "2.2.2.366"
+#define STRPRODUCTVER "2.2.2.366"
+#define BUILD_ID 20151002173842
+#define STRBUILD_ID "20151002173842"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
