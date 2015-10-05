76540ca2bb5acaf3dbb3899215c78be7ae54e9f9
Ryuichiro Ishitani <you@example.com>
2015-10-05 18:35:49 +0900

    added toppar debug msg

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,367
-#define PRODUCTVER    2,2,2,367
-#define STRFILEVER    "2.2.2.367"
-#define STRPRODUCTVER "2.2.2.367"
-#define BUILD_ID 20151005131849
-#define STRBUILD_ID "20151005131849"
+#define FILEVER       2,2,2,368
+#define PRODUCTVER    2,2,2,368
+#define STRFILEVER    "2.2.2.368"
+#define STRPRODUCTVER "2.2.2.368"
+#define BUILD_ID 20151005182217
+#define STRBUILD_ID "20151005182217"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
