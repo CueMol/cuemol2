da8119afef13f5d99feaaca1aa24278702dae6dd
Your Name <you@example.com>
2015-10-24 21:49:40 +0900

    pymodule impl

---
+++ src/pybr/initcuemol.cpp
@@ -0,0 +1,119 @@
+//
+// Object wrapper for PyObject
+//
+
+#include <Python.h>
+
+#include <common.h>
+#include <qlib/qlib.hpp>
+#include <qlib/LDebug.hpp>
+
+#include <qsys/qsys.hpp>
+#include <sysdep/sysdep.hpp>
+
+#include "wrapper.hpp"
+
+using namespace pybr;
+
+/// DLL entrance routine
+PyMODINIT_FUNC
+initcuemol()
+{
+  qlib::init();
+  MB_DPRINTLN("CueMol2 XPCOM : INITIALIZED");
+  Wrapper::setup();
+}
+
+////////////////////////////////////////////////
+
+namespace render {
+  extern bool init();
+  extern void fini();
+}
+
+namespace molstr {
+  extern bool init();
+  extern void fini();
+}
+
+namespace molvis {
+  extern bool init();
+  extern void fini();
+}
+
+namespace xtal {
+  extern bool init();
+  extern void fini();
+}
+
+namespace molanl {
+  extern bool init();
+  extern void fini();
+}
+
+namespace surface {
+  extern bool init();
+  extern void fini();
+}
+
+namespace symm {
+  extern bool init();
+  extern void fini();
+}
+
+namespace lwview {
+  extern bool init();
+  extern void fini();
+}
+
+namespace anim {
+  extern bool init();
+  extern void fini();
+}
+
+namespace mdtools {
+  extern bool init();
+  extern void fini();
+}
+
+namespace importers {
+  extern bool init();
+  extern void fini();
+}
+
+
+namespace pybr {
+/// CueMol initialization routine
+PyObject *initCueMol(PyObject *self, PyObject *args)
+{
+  const char *config;
+
+  if (!PyArg_ParseTuple(args, "s", &config))
+    return NULL;
+  
+  qsys::init(config);
+  sysdep::init();
+
+  // load other modules
+  render::init();
+  molstr::init();
+  molvis::init();
+  xtal::init();
+  symm::init();
+  surface::init();
+  molanl::init();
+  lwview::init();
+  anim::init();
+  mdtools::init();
+  importers::init();
+
+  // initTextRender();
+  // MB_DPRINTLN("---------- initTextRender() OK");
+
+  // // setup timer
+  // qlib::EventManager::getInstance()->initTimer(new XPCTimerImpl);
+
+  return Py_BuildValue("");
+}
+}
+
