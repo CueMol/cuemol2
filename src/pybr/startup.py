f2d4ef85663b7c99733529c8fe3be6c9ea51172b
Your Name <you@example.com>
2015-10-25 23:07:39 +0900

    _

--- src/pybr/startup.py
+++ src/pybr/startup.py
@@ -12,8 +12,15 @@ cuemol.initCueMol(confpath)
 
 evtmgr = cuemol.getService("ScrEventManager")
 
-def listener(arg0, arg1, arg2, arg3, arg4, arg5):
-    print "event listener called!!"
+def listener(aSlotID, aCatStr, aTgtTypeID, aEvtTypeID, aSrcID, aInfoStr):
+    print "Event listener called!!"
+    print "  slot ID="+str(aSlotID)
+    print "  cat str="+aCatStr
+    print "  target ID="+str(aTgtTypeID)
+    print "  event ID="+str(aEvtTypeID)
+    print "  src ID="+str(aSrcID)
+    print "  info : "+aInfoStr
+#    print "  conf path="+confpath
 
 evtmgr.addListener(listener)
 evtmgr.append("log", -1, -1, -1)
@@ -21,4 +28,15 @@ evtmgr.append("log", -1, -1, -1)
 logMgr = cuemol.getService("MsgLog")
 accumMsg = logMgr.getAccumMsg()
 logMgr.removeAccumMsg()
+
+from PyQt5.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+w = QWidget()
+w.resize(250, 150)
+w.move(300, 300)
+w.setWindowTitle('Simple')
+w.show()
+
+sys.exit(app.exec_())
 
