f67d5fc55ae2475ccca222592d64db4660f95234
Ryuichiro Ishitani <you@example.com>
2015-12-21 23:35:25 +0900

    ux py3 impl

--- src/qt_gui/run.sh
+++ src/qt_gui/run.sh
@@ -3,5 +3,5 @@
 SRCDIR=$HOME/proj64/trtiyam/cuemol2/src
 
 env PYTHONPATH=$SRCDIR/qt_gui/lib \
-  python -i startup.py $SRCDIR/xul_gui/sysconfig.xml
+  python3 -i startup.py $SRCDIR/xul_gui/sysconfig.xml
 
