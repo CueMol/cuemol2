961e69462a4402874d27c182f5ececea6fc11a72
Your Name <you@example.com>
2016-01-31 22:22:45 +0900

    _

---
+++ src/pyqt_gui/pylib/__init__.py
@@ -0,0 +1,9 @@
+from . import command, scene, fileio
+
+command.register("bg_color", scene.bg_color, [command.AT_COLSTR, command.AT_SCENENAME], "change background color of scene")
+command.register("undo", scene.undo, [command.AT_SCENENAME], "undo operation")
+command.register("redo", scene.redo, [command.AT_SCENENAME], "redo operation")
+
+command.register("load", fileio.load, [command.AT_PATH, command.AT_STRING, command.AT_STRING, command.AT_INTEGER], "load scene or object")
+
+
