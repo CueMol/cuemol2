63aed9eeacc4c4a3efb93fce2b53634893df9f56
Your Name <you@example.com>
2016-09-30 16:29:34 +0900

    _

--- src/modules/molvis/Spline2Renderer.hpp
+++ src/modules/molvis/Spline2Renderer.hpp
@@ -276,20 +276,13 @@ namespace molvis {
   class Spline2Renderer;
   class Spline2Seg;
 
-  class Spl2DrawSeg
+  class Spl2DrawSeg : public detail::DrawSegment
   {
-  private:
-    int m_nStart;
-    int m_nEnd;
-
-    /// tesselation level detail (copy of rend's prop)
-    int m_nDetail;
-
-    /// size of vertex/attribute array
-    int m_nVA;
-
   public:
-    Spl2DrawSeg(int st, int en) : m_nStart(st), m_nEnd(en), m_pVBO(NULL), m_pAttrAry(NULL)
+
+    typedef detail::DrawSegment super_t;
+
+    Spl2DrawSeg(int st, int en) : super_t(st,en), m_pVBO(NULL), m_pAttrAry(NULL)
     {
     }
 
@@ -300,14 +293,6 @@ namespace molvis {
 
     /// cached vertex array/VBO
     gfx::DrawElemVC *m_pVBO;
-
-    void setupVBO(Spline2Renderer *pthis);
-
-    void updateVBO(CubicSpline *pCoeff);
-
-    void updateVBOColor(Spline2Renderer *pthis, Spline2Seg *pseg);
-
-    void drawVBO(Spline2Renderer *pthis, DisplayContext *pdc);
 
     //////////
     // GLSL implementation
@@ -322,125 +307,33 @@ namespace molvis {
     /// VBO for glsl rendering
     AttrArray *m_pAttrAry;
 
-    /// Initialize shaders/texture
-    void setupGLSL(Spline2Renderer *pthis);
-
-    void updateGLSLColor(Spline2Renderer *pthis, Spline2Seg *pSeg);
-
-    /// display() for GLSL version
-    void drawGLSL(DisplayContext *pdc);
-
   };
-
 
   //
   /// Rendering object for the one spline segment
   //
-  class Spline2Seg
+  class Spline2Seg : public detail::SplineSegment
   {
-  private:
+  public:
+
+    typedef detail::SplineSegment super_t;
 
     typedef std::deque<Spl2DrawSeg> Spl2DrawList;
     Spl2DrawList m_draws;
 
-    typedef std::vector<int> IDArray;
-    
-    /// Pivot atom AID array
-    IDArray m_aids;
-
-    /// Pivot atom crd array index (for dynamic update)
-    IDArray m_inds;
-
-    std::deque<int> m_aidtmp;
-
-  public:
-    Spline2Seg();
-    ~Spline2Seg();
-
-    void append(MolAtomPtr pAtom);
-
-    void generate(Spline2Renderer *pthis, DisplayContext *pdc);
-
-    quint32 getSize() const { return m_scoeff.getSize(); }
-
-    MolAtomPtr getAtom(MolCoordPtr pMol, quint32 ind) const {
-      quint32 aid = m_aids[ind];
-      return pMol->getAtom(aid);
+    Spline2Seg() : super_t()
+    {
+      m_pCoefTex = NULL;
     }
+    virtual ~Spline2Seg();
 
-    MolResiduePtr getResid(MolCoordPtr pMol, quint32 ind) const {
-      MolAtomPtr pAtom = getAtom(pMol, ind);
-      return pAtom->getParentResidue();
-    }
+    virtual void generateImpl(int nstart, int nend);
 
-    quint32 calcColor(Spline2Renderer *pthis, MolCoordPtr pMol, float par) const;
-
-    //////////
-    // drawing methods
-
-    void updateDynamic(Spline2Renderer *pthis);
-    
-    void updateStatic(Spline2Renderer *pthis);
-    
-    void updateColor(Spline2Renderer *pthis);
-
-    void draw(Spline2Renderer *pthis, DisplayContext *pdc);
-    
-  private:
-
-    //////////
-    // spline methods
-
-    CubicSpline m_scoeff;
-
-    // int m_nPoints;
-    
-    void updateScoeffDynamic(Spline2Renderer *pthis);
-    void updateScoeffStatic(Spline2Renderer *pthis);
-
-    //////////
-    // VBO implementation
-
-    void setupVBO(Spline2Renderer *pthis);
-
-    void updateDynamicVBO(Spline2Renderer *pthis);
-
-    void updateStaticVBO(Spline2Renderer *pthis);
-
-    void updateVBOColor(Spline2Renderer *pthis);
-
-    void drawVBO(Spline2Renderer *pthis, DisplayContext *pdc);
-
-    void updateVBO();
-
-  private:
     /////////////////////
     // GLSL implementation
 
-    /// coeff float texture
+    /// float texture of the main axis coeff (common)
     gfx::Texture *m_pCoefTex;
-
-    std::vector<float> m_coefbuf;
-
-    //
-    // GLSL methods
-    //
-
-    /// Initialize shaders/texture
-    void setupGLSL(Spline2Renderer *pthis, DisplayContext *pdc);
-
-    /// update coord texture for GLSL rendering
-    void updateDynamicGLSL(Spline2Renderer *pthis);
-
-    void updateStaticGLSL(Spline2Renderer *pthis);
-
-    void updateGLSLColor(Spline2Renderer *pthis);
-
-    /// display() for GLSL version
-    void drawGLSL(Spline2Renderer *pthis, DisplayContext *pdc);
-
-    void updateCoefTex();
-
   };
 
   typedef std::deque<Spline2Seg> Spl2SegList;
@@ -450,27 +343,15 @@ namespace molvis {
   // Spline Renderer version 2 class
   //
 
-  class Spline2Renderer : public MainChainRenderer
+  class Spline2Renderer : public SplineRendBase
   {
     MC_SCRIPTABLE;
     MC_CLONEABLE;
 
-    typedef MainChainRenderer super_t;
+    typedef SplineRendBase super_t;
 
     //////////////
     // Properties
-
-  private:
-    /// Num of interporation point to the axial direction (axialdetail)
-    int m_nAxialDetail;
-
-  public:
-    void setAxialDetail(int nlev) {
-      m_nAxialDetail = nlev;
-      // invalidateSplineCoeffs();
-    }
-
-    int getAxialDetail() const { return m_nAxialDetail; }
 
   private:
     /// width of line drawing (in pixel unit)
@@ -498,14 +379,14 @@ namespace molvis {
     
     virtual const char *getTypeName() const;
 
-    virtual void display(DisplayContext *pdc);
+    // virtual void display(DisplayContext *pdc);
 
     /////////////////
     // DispCacheRenderer interface
 
     virtual void preRender(DisplayContext *pdc);
     
-    void invalidateDisplayCache();
+    // void invalidateDisplayCache();
 
 
     /////////////////
@@ -513,30 +394,45 @@ namespace molvis {
 
     virtual void propChanged(qlib::LPropEvent &ev);
 
-    virtual void objectChanged(qsys::ObjectEvent &ev);
+    // virtual void objectChanged(qsys::ObjectEvent &ev);
 
+
+  public:
+    /////////////////
+    // Common implementation
+
+    virtual void createSegList(DisplayContext *pdc);
+
+    virtual SplineSegment *createSegment();
 
     /////////////////
-    // work area
+    // VBO implementation
 
-  private:
+    virtual void setupVBO(detail::SplineSegment *pSeg, DisplayContext *pdc);
 
-    Spl2SegList m_seglist;
+    virtual void updateCrdVBO(detail::SplineSegment *pSeg);
 
-    void createSegList(DisplayContext *pdc);
+    virtual void updateColorVBO(detail::SplineSegment *pSeg, DisplayContext *pdc);
 
-    void startColorCalc();
-    void endColorCalc();
-
+    virtual void drawVBO(detail::SplineSegment *pSeg, DisplayContext *pdc);
 
     /////////////////
     // GLSL implementation
 
-  public:
-    bool m_bUseGLSL;
+    /// Initialize shaders
+    virtual bool initShader(DisplayContext *pdc);
 
-    /// shader check was performed
-    bool m_bChkShaderDone;
+    virtual void setupGLSL(detail::SplineSegment *pSeg, DisplayContext *pdc);
+
+    virtual void updateCrdGLSL(detail::SplineSegment *pSeg);
+
+    virtual void updateColorGLSL(detail::SplineSegment *pSeg, DisplayContext *pdc);
+
+    virtual void drawGLSL(detail::SplineSegment *pSeg, DisplayContext *pdc);
+
+  private:
+    /////////////////
+    // work area
 
     /// GLSL shader objects
     sysdep::OglProgramObject *m_pPO;
@@ -544,9 +440,6 @@ namespace molvis {
     quint32 m_nRhoLoc;
 
     quint32 m_nColLoc;
-
-    /// Initialize shaders
-    void initShader(DisplayContext *pdc);
 
   };
 
