fc3d834d5f8f572c45718118bf898f995878d2cb
Your Name <you@example.com>
2016-10-27 13:26:56 +0900

    win64 support

--- winbuild/installer/iss-version.pl
+++ winbuild/installer/iss-version.pl
@@ -3,19 +3,35 @@
 # $Id: iss-version.pl,v 1.2 2011/02/15 10:05:38 rishitani Exp $
 #
 # Embed the version number into InnoSetup iss file
-# usage: perl <InnoSetup input file> <version.hpp file> <xulrunner_bindir> <proj_dir>
+# usage: perl <InnoSetup input file> <version.hpp file> <xulrunner_dir> <buildset_dir> <xuldeploy_dir> <boost_version_str>
 
 use strict;
 
 my $input_iss = $ARGV[0];
 my $version_file = $ARGV[1];
 my $xulbin_dir = $ARGV[2]."\\bin";
-my $proj_dir = $ARGV[3];
+my $buildset_dir = $ARGV[3];
+my $xuldeploy_dir = $ARGV[4];
+my $boost_ver = $ARGV[5];
 
 print "xulbin_dir=$xulbin_dir\n";
 die unless (-d $xulbin_dir);
-print "proj_dir=$proj_dir\n";
-die unless (-d $proj_dir);
+
+print "buildset_dir=$buildset_dir\n";
+die unless (-d $buildset_dir);
+
+print "xuldeploy_dir=$xuldeploy_dir\n";
+die unless (-d $xuldeploy_dir);
+
+my $platform="win32";
+if ($xuldeploy_dir=~/64/) {
+  $platform="x64";
+}
+
+# check boost lib location
+print "boost_version_str=$boost_ver\n";
+print "check file: $buildset_dir\\bin\\boost_thread-$boost_ver.dll\n";
+die unless (-f "$buildset_dir\\bin\\boost_thread-$boost_ver.dll");
 
 # extract version no
 open(IN, $version_file) || die "$version_file : $!";
@@ -31,61 +47,32 @@ while (<IN>) {
 }
 close(IN);
 
-#if (length($verstring)<=0) {
-#    die "cannot extract version string from $version_file";
-#}
-
-# replace version string
-# open(IN, $input_iss) || die "$input_iss : $!";
-# open(OUT, ">$output_iss") || die "$output_iss : $!";
-# 
-# print "PROJ_DIR=$proj_dir\n";
-# my $accum="";
-# while (<IN>) {
-#   s/\@VERSION_RELEASE_ID\@/$release_ID/g;
-#   s/\@VERSION_BUILD_ID\@/$build_ID/g;
-#   if ($proj_dir) {
-#     s/\@PROJ_DIR\@/$proj_dir/g;
-#   }
-#   
-#     if (/(.*)\\$/) {
-#         $accum .= $1;
-#     }
-#     else {
-#         print OUT $accum;
-#         $accum="";
-#         print OUT $_;
-#     }
-# }
-# 
-# print OUT "$accum\n";
-# close(IN);
-# close(OUT);
-
-my $povray_cmd = "";
-if (-f "$proj_dir/povray-bundle/povray.exe" &&
-    -d "$proj_dir/povray-bundle/include" ) {
-  $povray_cmd = "\"/dPovBundleDir=$proj_dir/povray-bundle\"";
+my $povray_cmd = " ";
+if (-f "$buildset_dir/povray-bundle/povray.exe" &&
+    -d "$buildset_dir/povray-bundle/include" ) {
+  $povray_cmd = "\"/dPovBundleDir=$buildset_dir/povray-bundle\" ";
 }
 
-my $ffmpeg_cmd = "";
-if (-f "$proj_dir/ffmpeg-bundle/bin/ffmpeg.exe") {
-  $ffmpeg_cmd = "\"/dFFmpegBundleDir=$proj_dir/ffmpeg-bundle\"";
+my $ffmpeg_cmd = " ";
+if (-f "$buildset_dir/ffmpeg-bundle/bin/ffmpeg.exe") {
+  $ffmpeg_cmd = "\"/dFFmpegBundleDir=$buildset_dir/ffmpeg-bundle\" ";
 }
 
-my $apbs_cmd = "";
-if (-d "$proj_dir/apbs-bundle") {
-  $apbs_cmd = "\"/dAPBSBundleDir=$proj_dir/apbs-bundle\"";
+my $apbs_cmd = " ";
+if (-d "$buildset_dir/apbs-bundle") {
+  $apbs_cmd = "\"/dAPBSBundleDir=$buildset_dir/apbs-bundle\" ";
 }
 
-my $cmd = "iscc \"/dPROJ_DIR=$proj_dir\" \"/dXulRTDir=$xulbin_dir\" \"/dCueMolReleaseID=$release_ID\" \"/dCueMolBuildID=$build_ID\" $povray_cmd $ffmpeg_cmd $apbs_cmd $input_iss";
+my $cmd = "iscc ".
+"\"/dPlatform=$platform\" ".
+"\"/dBUILDSET_DIR=$buildset_dir\" ".
+"\"/dXulRTDir=$xulbin_dir\" ".
+"\"/dCueMolReleaseID=$release_ID\" ".
+"\"/dCueMolBuildID=$build_ID\" ".
+"\"/dBoostVer=$boost_ver\" ".
+"\"/dWinBuildDir=$xuldeploy_dir\" ".
+$povray_cmd . $ffmpeg_cmd . $apbs_cmd . $input_iss;
+
 print("RUN: $cmd\n");
 system($cmd);
-
-#if ($povray_cmd) {
-#  # build non-pov version
-#  my $cmd = "iscc \"/dPROJ_DIR=$proj_dir\" \"/dCueMolReleaseID=$release_ID\" \"/dCueMolBuildID=$build_ID\" $input_iss";
-#  print("RUN: $cmd\n");
-#  system($cmd);
-#}
 
