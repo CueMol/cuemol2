c4b81a507b779e297179e7acfe73a533ee689110
Your Name <you@example.com>
2016-11-23 14:38:00 +0900

    luxrend bg impl

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,401
-#define PRODUCTVER    2,2,2,401
-#define STRFILEVER    "2.2.2.401"
-#define STRPRODUCTVER "2.2.2.401"
-#define BUILD_ID 20161108141650
-#define STRBUILD_ID "20161108141650"
+#define FILEVER       2,2,2,402
+#define PRODUCTVER    2,2,2,402
+#define STRFILEVER    "2.2.2.402"
+#define STRPRODUCTVER "2.2.2.402"
+#define BUILD_ID 20161123143757
+#define STRBUILD_ID "20161123143757"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
