3892f23d20789c968861c8500a1b51d25b5df850
Your Name <you@example.com>
2016-11-24 14:38:14 +0900

    macos build

--- src/version.hpp
+++ src/version.hpp
@@ -2,10 +2,10 @@
 // Version info
 //
 
-#define FILEVER       2,2,2,402
-#define PRODUCTVER    2,2,2,402
-#define STRFILEVER    "2.2.2.402"
-#define STRPRODUCTVER "2.2.2.402"
+#define FILEVER       2,2,3,402
+#define PRODUCTVER    2,2,3,402
+#define STRFILEVER    "2.2.3.402"
+#define STRPRODUCTVER "2.2.3.402"
 #define BUILD_ID 20161123143757
 #define STRBUILD_ID "20161123143757"
 
