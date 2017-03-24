666d76d4967aad1c0bb8bb9aa44c0ec9e5e56c93
Ryuichiro Ishitani <you@example.com>
2017-03-24 17:07:25 +0900

    luxrend exporter debug

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,3,408
-#define PRODUCTVER    2,2,3,408
-#define STRFILEVER    "2.2.3.408"
-#define STRPRODUCTVER "2.2.3.408"
-#define BUILD_ID 20170308132854
-#define STRBUILD_ID "20170308132854"
+#define FILEVER       2,2,3,409
+#define PRODUCTVER    2,2,3,409
+#define STRFILEVER    "2.2.3.409"
+#define STRPRODUCTVER "2.2.3.409"
+#define BUILD_ID 20170324170720
+#define STRBUILD_ID "20170324170720"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
