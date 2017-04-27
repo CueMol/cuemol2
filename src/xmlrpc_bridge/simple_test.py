103195a254d09d057d02149343edceee742541e5
Your Name <you@example.com>
2017-04-27 17:46:15 +0900

    xmlrpc debug

--- src/xmlrpc_bridge/simple_test.py
+++ src/xmlrpc_bridge/simple_test.py
@@ -18,8 +18,6 @@ if __name__ == "__main__":
         
         return
 
-#    test1()
-        
 ##############
 
     def test2():
@@ -34,5 +32,74 @@ if __name__ == "__main__":
         color.setCode(0xFFFFFF);
         sc.bgcolor = color;
         
-    test2()
+##############
+
+    def test3():
+        scm = cuemol.getService("SceneManager")
+        scid = scm.activeSceneID;
+        print "Active scene ID=", scid;
+        scene = scm.getScene(scid);
+        
+        #print "scene = "+str(scene)
+
+        mol = cuemol.createObj("MolCoord")
+        
+        fname = '/Users/user/Dropbox/works/test_data/1CRN.pdb';
+
+        for line in open(fname):
+            #print "PDB: ", line;
+            if line[0:6] in ["ATOM  ", "HETATM"]:
+                aname = line[12:16].strip(  )
+                altloc = line[16:17]
+                resn = line[17:20].strip(  )
+                chn = line[21:22]
+                resid = int( line[22:26] )
+                ins = line[26:27]
+                posx = float( line[30:38] )
+                posy = float( line[38:46] )
+                posz = float( line[46:54] )
+                occ = float( line[54:60] )
+                bfac = float( line[60:66] )
+                elem = line[76:78].strip()
+            else:        
+                print "PDB: ", line
+                continue
+
+            # print "name=", aname
+            atom = cuemol.createObj("MolAtom");
+            atom.name = aname;
+            atom.element = elem;
+            atom.bfac = bfac;
+            atom.occ = occ;
+        
+            vpos = cuemol.createObj("Vector");
+            vpos.set3(posx, posy, posz);
+            atom.pos = vpos;
+        
+            mol.appendAtom1(atom, chn, resid, resn);
+        
+        # apply topology
+        mol.applyTopology()
+        mol.name="Mol"
+
+        # calc 2nry str
+        mgr = cuemol.getService("MolAnlManager")
+	mgr.calcProt2ndry2(mol, True, 0.0);
+
+        # Register to the scene
+        scene.addObject(mol)
+        
+        # Create simple renderer
+        rend = mol.createRenderer("simple")
+        rend.name = "simple1"
+        rend.applyStyles("DefaultCPKColoring")
+
+        vwid = scene.activeViewID;
+        print "Active view ID=", vwid;
+        view = scm.getView(vwid);
+        view.setViewCenter( rend.getCenter() )
+
+        return
+
+    test3()
 
