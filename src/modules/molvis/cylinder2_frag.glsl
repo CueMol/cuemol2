ae6c1629ea728058ba33c30b229097ea5248fcc5
Your Name <you@example.com>
2017-05-07 17:13:36 +0900

    _

---
+++ src/modules/molvis/cylinder2_frag.glsl
@@ -0,0 +1,113 @@
+// -*-Mode: C++;-*-
+//
+//  fragment shader for cylinders
+//
+
+@include "lib_common.glsl"
+
+varying vec4 v_color;
+varying vec2 v_impos;
+varying vec4 v_ecpos;
+varying float v_ndec;
+//varying float v_dec;
+//varying float vw_len;
+//varying float v_sinph;
+varying float v_flag;
+
+uniform float frag_alpha;
+
+varying float v_depmx;
+varying vec2 v_normadj;
+varying vec2 v_vwdir;
+
+void main()
+{
+  float adj_cen = sqrt(1.0 - v_impos.x*v_impos.x);
+  float disp_cir = adj_cen * v_ndec;
+
+  float imy = v_impos.y;
+
+  /*
+  if (v_flag>0)
+    imy -= disp_cir;
+  else
+    imy += disp_cir;
+*/
+
+  imy -= disp_cir * v_flag;
+  
+  // discard the impostor pixels out of the cylinder
+  if (imy <= -1.0 ||
+      1.0 <= imy) {
+    discard;
+    return;
+  }
+
+  float depth = v_depmx * adj_cen;
+
+  vec3 normal = vec3(v_impos.x, v_normadj.x * adj_cen, v_normadj.y * adj_cen);
+
+  mat2 rmat = mat2(v_vwdir.x, v_vwdir.y,
+                   -v_vwdir.y, v_vwdir.x);
+
+  normal.xy *= rmat;
+
+  //normal.xy = rmat * normal.xy;
+  //float tx =  v_vwdir.x*normal.x + v_vwdir.y*normal.y;
+  //float ty = -v_vwdir.y*normal.x + v_vwdir.x*normal.y;
+  //normal.x = tx;
+  //normal.y = ty;
+  
+  float far=gl_DepthRange.far;
+  float near=gl_DepthRange.near;
+  
+  vec4 ecpos = v_ecpos;
+  ecpos.z += depth;
+  vec4 clip_space_pos = gl_ProjectionMatrix * ecpos;
+  
+  float ndc_depth = clip_space_pos.z / clip_space_pos.w;
+  
+  float fd = (((far-near) * ndc_depth) + near + far) / 2.0;
+  
+  // re-apply clipping by the view volume
+  if (fd>far) {
+    discard;
+  }
+  else if (fd<near) {
+    discard;
+    //normal = vec3(0.0, 0.0, 1.0);
+    //fd = near;
+  }
+  else {
+    gl_FragDepth = fd;
+    
+    // color calculation
+    vec4 color = v_color;
+    //float vis = (normal.y + 1.0)/2.0;
+    //vec4 color = vec4(vis, 0.0, 1.0-vis, 1.0);
+
+    color = flight(normal, ecpos, color);
+    
+    // fog calculation
+    float fogz = abs(ecpos.z);
+    float fog;
+    fog = (gl_Fog.end - fogz) * gl_Fog.scale;
+    fog = clamp(fog, 0.0, 1.0);
+    color = vec4(mix( vec3(gl_Fog.color), vec3(color), fog), v_color.a*frag_alpha);
+    
+    gl_FragColor = color;
+  }
+
+  //gl_FragColor = v_color;
+
+
+/*
+  float dist = length(v_impos);
+  if (dist>1.0) {
+    discard;
+  }
+  else {
+  }
+*/
+}
+
---
+++ src/modules/molvis/cylinder2_vert.glsl
@@ -0,0 +1,110 @@
+// -*-Mode: C++;-*-
+//
+//  vertex shader for cylinders (ver. 2)
+//
+
+////////////////////
+// Vertex attributes
+
+// position
+attribute vec3 a_vertex;
+
+// direction (oth-pos)
+attribute vec3 a_dir;
+
+// radius
+attribute float a_radius;
+
+// color
+attribute vec4 a_color;
+
+// impostor
+attribute vec2 a_impos;
+
+////////////////////
+// Uniform variables
+
+// Model-View projection matrix
+// uniform mat4 mvp_matrix;
+
+////////////////////
+// Varying variables
+
+varying vec4 v_color;
+varying vec2 v_impos;
+varying vec4 v_ecpos;
+
+varying float v_ndec;
+//varying float v_dec;
+//varying float vw_len;
+//varying float v_sinph;
+varying float v_flag;
+varying float v_depmx;
+varying vec2 v_normadj;
+varying vec2 v_vwdir;
+
+////////////////////
+// Program
+
+void main()
+{
+  vec4 ec_tpos = gl_ModelViewMatrix * vec4(a_vertex, 1.0);
+  vec4 ec_opos = gl_ModelViewMatrix * vec4((a_vertex+a_dir), 1.0);
+  
+  vec4 ec_dir = ec_opos - ec_tpos;
+  vec2 n_vwdir = normalize(ec_dir.xy);
+  vec3 n_ecdir = normalize(ec_dir.xyz);
+  
+  vec4 iec_dir = ec_dir * a_impos.y;
+  vec2 n_ivwdir = normalize(iec_dir.xy);
+  vec3 n_iecdir = normalize(iec_dir.xyz);
+
+  float len = length(ec_dir.xyz);
+  float vw_len = length(ec_dir.xy);
+  //vec2 rf = ec_dir.xy/vw_len;
+
+  float sinph = ec_dir.z / len;
+  float rcosph = inversesqrt(1.0-sinph*sinph);
+  float tanph = sinph * rcosph;
+  float dec = a_radius * sinph;
+  vec3 dec_dir = a_radius * tanph * n_ecdir;
+
+  v_vwdir = n_ivwdir;
+  v_ndec = 2.0 * abs(dec) / vw_len;
+
+  v_flag = sign( iec_dir.z );
+  //v_sinph = sinph;
+  
+  v_depmx = a_radius * rcosph;
+
+  v_normadj = vec2(-sinph * a_impos.y, 1.0/rcosph);
+
+  /////
+  
+  //v_ecpos = ec_tpos + vec4(vert_dsp, 0.0);
+  //gl_Position = gl_ProjectionMatrix * v_ecpos;
+
+  vec3 vert_dsp = vec3(a_impos.x * -n_ivwdir.y*a_radius,
+                       a_impos.x *  n_ivwdir.x*a_radius,
+                       0);
+  if ( dec > 0.0 ) {
+    // extend the end of the cap
+    vert_dsp -= dec_dir;
+    v_impos = a_impos + vec2(0, v_ndec * a_impos.y);
+    //v_impos = a_impos;
+  }
+  else {
+    v_impos = a_impos;
+  }
+
+  vec4 ec_pos_dsp = ec_tpos + vec4(vert_dsp, 0.0);
+  gl_Position = gl_ProjectionMatrix * ec_pos_dsp;
+
+  v_color = a_color;
+  v_ecpos = ec_pos_dsp;
+
+  //gl_Position = vec4(0,0,0,0);
+  //v_color = vec4(1,1,1,1);
+  // gl_FrontColor = gl_Color;
+}
+
