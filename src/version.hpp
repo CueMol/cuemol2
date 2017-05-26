8cfe5eea81b21d3fd3c3306c638bfd695904277c
Your Name <you@example.com>
2017-05-26 22:02:06 +0900

    _

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,3,414
-#define PRODUCTVER    2,2,3,414
-#define STRFILEVER    "2.2.3.414"
-#define STRPRODUCTVER "2.2.3.414"
-#define BUILD_ID 20170426145115
-#define STRBUILD_ID "20170426145115"
+#define FILEVER       2,2,3,415
+#define PRODUCTVER    2,2,3,415
+#define STRFILEVER    "2.2.3.415"
+#define STRPRODUCTVER "2.2.3.415"
+#define BUILD_ID 20170526220159
+#define STRBUILD_ID "20170526220159"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
