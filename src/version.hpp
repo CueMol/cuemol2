230c123b2befdc3153ce7b31d973b1edb9611c3a
Ryuichiro Ishitani <you@example.com>
2017-06-29 15:54:36 +0900

    merge python36 branch

--- src/version.hpp
+++ src/version.hpp
@@ -2,12 +2,12 @@
 // Version info
 //
 
-#define FILEVER       2,2,3,415
-#define PRODUCTVER    2,2,3,415
-#define STRFILEVER    "2.2.3.415"
-#define STRPRODUCTVER "2.2.3.415"
-#define BUILD_ID 20170526220159
-#define STRBUILD_ID "20170526220159"
+#define FILEVER       2,2,3,416
+#define PRODUCTVER    2,2,3,416
+#define STRFILEVER    "2.2.3.416"
+#define STRPRODUCTVER "2.2.3.416"
+#define BUILD_ID 20170629155423
+#define STRBUILD_ID "20170629155423"
 
 #define QUE_VERSION_STRING "Version " STRPRODUCTVER " build " STRBUILD_ID
 
