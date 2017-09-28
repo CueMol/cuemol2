57cf41c5dc6d1cf133a6e310b968e1bc16f1c73f
Ryuichiro Ishitani <you@example.com>
2017-09-28 18:00:47 +0900

    atomintr2 impl

---
+++ src/modules/molvis/stpline1_frag.glsl
@@ -0,0 +1,36 @@
+// -*-Mode: C++;-*-
+//
+//  Stipple line fragment shader for OpenGL (ver.1)
+//
+
+@include "lib_common.glsl"
+
+////////////////////
+// Varying variables
+
+varying float v_linepos;
+
+////////////////////
+// Uniform variables
+
+// label color (incl. transparency)
+uniform vec4 u_color;
+
+// stipple pattern
+uniform vec2 u_stipple;
+
+void main (void)
+{
+  float i = v_linepos/(u_stipple.x + u_stipple.y);
+  float rho = i - floor(i);
+
+  float thr = u_stipple.x/(u_stipple.x + u_stipple.y);
+
+  if (rho < thr) {
+    gl_FragColor = u_color;
+  }
+  else {
+    gl_FragColor = vec4(u_color.xyz, 0.0f);
+  }
+}
+
---
+++ src/modules/molvis/stpline1_vert.glsl
@@ -0,0 +1,49 @@
+// -*-Mode: C++;-*-
+//
+//  Stipple-line vertex shader for OpenGL (ver.1)
+//
+
+@include "lib_common.glsl"
+
+////////////////////
+// Vertex attributes
+
+attribute vec3 a_pos1;
+attribute vec3 a_pos2;
+attribute float a_hwidth;
+attribute float a_dir;
+
+////////////////////
+// Uniform variables
+
+//uniform vec2 u_winsz;
+//uniform float u_ppa;
+
+////////////////////
+// Varying variables
+
+varying float v_linepos;
+
+void main (void)
+{
+  vec3 v12 = a_pos2 - a_pos1;
+  
+  vec4 ec_v12 = gl_ModelViewMatrix * vec4(v12, 0.0f);
+
+  vec2 e1 = normalize(ec_v12.xy);
+  vec2 e2 = vec2(-e1.y, e1.x);
+  
+  vec4 ecPosition = gl_ModelViewMatrix * vec4(a_pos1, 1.0);
+
+  ecPosition.xy += e2 * a_hwidth;
+
+  // Do fixed functionality vertex transform
+  gl_Position = gl_ProjectionMatrix * ecPosition;
+
+  gl_FrontColor=gl_Color;
+
+  gl_FogFragCoord = ffog(ecPosition.z);
+
+  v_linepos = a_dir * length(v12);
+}
+
