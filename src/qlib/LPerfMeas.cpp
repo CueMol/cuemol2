eae9cdf83d8c9c5980ed628c6334b472378bbc53
Ryuichiro Ishitani <you@example.com>
2018-01-17 17:35:25 +0900

    _

---
+++ src/qlib/LPerfMeas.cpp
@@ -0,0 +1,113 @@
+//
+// Performance measurement manager implementation
+//
+
+#include <common.h>
+
+#include "LPerfMeas.hpp"
+
+#ifdef USE_BOOST_TIMER
+#include <boost/timer/timer.hpp>
+#endif  
+
+namespace qlib {
+  SINGLETON_BASE_IMPL(PerfMeasManager);
+}
+
+using namespace qlib;
+
+PerfMeasManager::PerfMeasManager()
+     : m_busytimes(NAVER_SIZE), m_nBusyTimeIndex(0), m_nActiveTimerID(-1)
+{
+#ifdef USE_BOOST_TIMER
+  m_pTimer = MB_NEW boost::timer::cpu_timer();
+#endif
+}
+
+PerfMeasManager::~PerfMeasManager()
+{
+#ifdef USE_BOOST_TIMER
+  boost::timer::cpu_timer *pTimer = static_cast<boost::timer::cpu_timer *>(m_pTimer);
+  delete pTimer;
+#endif
+}
+
+void PerfMeasManager::enable(int nID)
+{
+  m_nActiveTimerID = nID;
+}
+
+void PerfMeasManager::disable()
+{
+  m_nActiveTimerID = -1;
+}
+
+void PerfMeasManager::setBusyTime(quint64 nanosec)
+{
+  m_busytimes[m_nBusyTimeIndex] = nanosec;
+  m_nBusyTimeIndex ++;
+  if (m_nBusyTimeIndex>=NAVER_SIZE) {
+    m_nBusyTimeIndex = 0;
+    double aver = 0.0;
+    for (int i=0; i<NAVER_SIZE; ++i) {
+      aver += double(m_busytimes[i]);
+    }
+    aver /= double(NAVER_SIZE);
+    LOG_DPRINTLN("Average busy time for ID=%d: %f microsec (FPS=%f)", m_nActiveTimerID, aver/1000.0, 1000.0*1000.0*1000.0/aver);
+  }
+}
+
+void PerfMeasManager::start(int nID)
+{
+  if (nID!=m_nActiveTimerID)
+    return;
+  
+#ifdef USE_BOOST_TIMER
+  boost::timer::cpu_timer *pTimer = static_cast<boost::timer::cpu_timer *>(m_pTimer);
+  pTimer->start();
+#endif
+}
+
+void PerfMeasManager::end(int nID)
+{
+  if (nID!=m_nActiveTimerID)
+    return;
+
+#ifdef USE_BOOST_TIMER
+  boost::timer::cpu_timer *pTimer = static_cast<boost::timer::cpu_timer *>(m_pTimer);
+  pTimer->stop();
+  boost::timer::cpu_times t = pTimer->elapsed();
+  setBusyTime(t.wall);
+#endif
+}
+
+//////////
+
+AutoTimeMeas::AutoTimeMeas(const char *msg)
+{
+  if (msg!=NULL)
+    m_msg = msg;
+#ifdef USE_BOOST_TIMER
+  boost::timer::cpu_timer *p = new boost::timer::cpu_timer();
+  p->start();
+  m_pTimerObj = p;
+#endif
+}
+
+AutoTimeMeas::~AutoTimeMeas()
+{
+#ifdef USE_BOOST_TIMER
+  boost::timer::cpu_timer *p = static_cast<boost::timer::cpu_timer *>(m_pTimerObj);
+  boost::timer::cpu_times t = p->elapsed();
+  delete p;
+  m_pTimerObj = NULL;
+  
+  LString msg = boost::timer::format(t);
+  msg = msg.chomp();
+  
+  LOG_DPRINTLN( "%s> %s",
+                m_msg.c_str(),
+                msg.c_str() );
+#endif
+}
+
---
+++ src/qlib/LPerfMeas.hpp
@@ -0,0 +1,101 @@
+// -*-Mode: C++;-*-
+//
+//  Performance measurement using boost::timer
+//
+
+#ifndef QLIB_PERF_MEAS_HPP
+#define QLIB_PERF_MEAS_HPP
+
+#include "qlib.hpp"
+#include "LTypes.hpp"
+#include "LString.hpp"
+#include "SingletonBase.hpp"
+
+#define PM_IDLE_TIMER 0
+#define PM_DRAW_SCENE 1
+#define PM_LABEL_RENDER 2
+#define PM_RENDER_SCENE 3
+
+namespace qlib {
+
+  class QLIB_API PerfMeasManager : public SingletonBase<PerfMeasManager>
+  {
+  private:
+    static const int NAVER_SIZE = 100;
+
+    std::vector<qint64> m_busytimes;
+    void *m_pTimer;
+    int m_nBusyTimeIndex;
+
+    int m_nActiveTimerID;
+    
+    void setBusyTime(quint64 nanosec);
+
+  public:
+    
+    PerfMeasManager();
+    virtual ~PerfMeasManager();
+
+    void enable(int naver);
+    void disable();
+
+
+    void start(int nID);
+    void end(int nID);
+
+  };
+  
+  /// Auto performance measurement class for iterative execution
+  class QLIB_API AutoPerfMeas
+  {
+  private:
+    int m_nID;
+    PerfMeasManager *m_pPM;
+
+  public:
+    AutoPerfMeas(int nID) : m_nID(nID), m_pPM(NULL)
+    {
+      BeginRequest();
+    }
+    
+    ~AutoPerfMeas()
+    {
+      EndRequest();
+    }
+    
+  private:
+
+    void EndRequest()
+    {
+      if (m_pPM==NULL)
+        m_pPM = PerfMeasManager::getInstance();
+      m_pPM->end(m_nID);
+    }
+    
+    void BeginRequest()
+    {
+      if (m_pPM==NULL)
+        m_pPM = PerfMeasManager::getInstance();
+      m_pPM->start(m_nID);
+    }
+  };
+  
+  /// Auto performance measurement class for single execution
+  class QLIB_API AutoTimeMeas
+  {
+  private:
+    void *m_pTimerObj;
+
+    LString m_msg;
+
+  public:
+    AutoTimeMeas(const char *msg=NULL);
+
+    ~AutoTimeMeas();
+
+  };
+
+}
+
+#endif
+
