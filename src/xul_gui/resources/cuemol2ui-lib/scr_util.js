a1e8035b8375e8b1b255369fddcf466e5fe4d11f
Ryuichiro Ishitani <you@example.com>
2018-01-22 20:48:46 +0900

    _

--- src/xul_gui/resources/cuemol2ui-lib/scr_util.js
+++ src/xul_gui/resources/cuemol2ui-lib/scr_util.js
@@ -93,3 +93,22 @@ exports.txn = function (aScene, aMsg, aFunc) {
   return true;
 };
 
+exports.filePicker = function (aWindow, aExt, aDesc)
+{
+  const nsIFilePicker = Ci.nsIFilePicker;
+  let fp = Cc["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+
+  fp.init(aWindow, "Select a File", nsIFilePicker.modeOpen);
+
+  if (aExt && aDesc) {
+    fp.appendFilter(aDesc, aExt);
+  }
+
+  let res = fp.show();
+  if (res!=nsIFilePicker.returnOK) {
+    return null;
+  }
+
+  return fp.file.path;
+}
+
