5cdd151eb4f29427d734da1a69c733cdab9240c7
user1 <user1@user1noMacBook-Pro.local>
2020-05-30 17:16:57 +0900

    load obj impl

---
+++ src/qsys/command/LoadObjectCommand.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <qsys/Scene.hpp>
+#include <qsys/View.hpp>
+#include <qsys/qsys.hpp>
+
+#include "Command.hpp"
+
+namespace qsys {
+
+using qlib::LString;
+
+/// Load object CLI command
+class QSYS_API LoadObjectCommand : public Command
+{
+    MC_SCRIPTABLE;
+    MC_CLONEABLE;
+
+private:
+
+public:
+    LoadObjectCommand() = default;
+    virtual ~LoadObjectCommand() = default;
+
+    /// Execute the command
+    virtual void run();
+
+    virtual void runGUI(void *pwnd_info);
+
+    /// Get command's unique name
+    virtual const char *getName() const;
+
+    LString guessFileFormat(int nCatID) const;
+
+    //////////
+    // properties (input)
+
+    /// Target scene
+    ScenePtr m_pTargScene;
+
+    /// object file path
+    LString m_filePath;
+
+    /// scene file format (optional)
+    LString m_fileFmt;
+
+    //////////
+    // properties (output)
+
+    /// resulting object
+    ObjectPtr m_pResObj;
+
+};
+
+}  // namespace qsys
