1aa38e6dca7eaf42ecc184ef260e471896b0cad1
user1 <user1@user1noMacBook-Pro.local>
2020-09-22 15:45:50 +0900

    add unix launch script

--- src/python/cuemol/internal_loader.py
+++ src/python/cuemol/internal_loader.py
@@ -2,6 +2,7 @@ import importlib
 
 _internal_module = None
 
+
 def import_internal():
     global _internal_module
     if _internal_module is not None:
@@ -11,13 +12,13 @@ def import_internal():
     if _internal_module is None:
         mod_name = "cuemol._cuemol_internal"
         try:
-            _internal_module = importlib.import_module(mod_name) 
+            _internal_module = importlib.import_module(mod_name)
         except ModuleNotFoundError:
             pass
 
     # try embedded version
     if _internal_module is None:
         mod_name = "_cuemol_internal"
-        _internal_module = importlib.import_module(mod_name) 
-        
+        _internal_module = importlib.import_module(mod_name)
+
     return _internal_module
