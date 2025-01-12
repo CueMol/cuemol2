e1fd23c90761717aff3e8ff332c074b8d54e0573
ishitani <ishitani@users.sourceforge.net>
2025-01-12 19:55:00 +0900

    remove unused script/bump version

--- build_scripts/launch_gui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TOP_DIR=$(cd $(dirname $0)/../; pwd)
-PYTHON=python3
-
-env CUEMOL_SYSCONFIG_PATH=$TOP_DIR/src/xul_gui \
-    PYTHONPATH=$TOP_DIR/src/python:$TOP_DIR/build:$TOP_DIR/build/python \
-    $PYTHON $TOP_DIR/src/python/cuemol_gui/startup.py
