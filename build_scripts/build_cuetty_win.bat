fb4d983e8a8ddcefdeb885348d49917ffa50ee4d
Ryuichiro Ishitani <r.ishitani@tmd.ac.jp>
2025-11-12 10:19:46 +0900

    fix win cuetty build batch file

--- build_scripts/build_cuetty_win.bat
+++ build_scripts/build_cuetty_win.bat
@@ -8,36 +8,50 @@ if "%1"=="" (
 SET BASEDIR=%1
 SET RUNNER_OS=Windows
 SET RUNNER_ARCH=X64
-SET TMPDIR=%BASEDIR%\tmp
 
-mkdir %TMPDIR%
-%~d1
-cd %TMPDIR%
+SET SCRIPT_DIR=%~dp0
+echo SCRIPT_DIR: %SCRIPT_DIR%
+if "%GITHUB_WORKSPACE%"=="" (
+   pushd %SCRIPT_DIR%\..
+   for /f %%i in ('cd') do set TOP_DIR=%%i
+   popd
+) ELSE (
+   SET TOP_DIR=%GITHUB_WORKSPACE%
+)
+echo TOP_DIR: %TOP_DIR%
 
-SET DEPLIBS_DIR=%BASEDIR%\proj64_deplibs
-echo "DEPLIBS_DIR:" %DEPLIBS_DIR%
+SET BOOST_VER=1_84_0
+
+REM Install location
+SET INSTPATH=%BASEDIR%\cuemol2
+
+REM Build
+SET BUILD_DIR=%BASEDIR%/build_cuetty
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+REM SET BUILD_TYPE=Debug
+SET BUILD_TYPE=Release
+
+dir %BASEDIR%
 
 REM Build cuetty
-SET INSTPATH=%DEPLIBS_DIR%\cuemol2
-rd /s /q build
+REM SET SCCACHE=-DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
+SET SCCACHE=
 
-SET CONFIG=Release
-SET SCCACHE=-DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
-SET BUILDDIR=build_cli
-
-cmake -G Ninja -S %GITHUB_WORKSPACE%\cli -B %BUILDDIR% ^
+cmake -G Ninja -S %TOP_DIR%\cli -B %BUILD_DIR% ^
  -DCMAKE_INSTALL_PREFIX=%INSTPATH% ^
- -DCMAKE_PREFIX_PATH=%DEPLIBS_DIR% ^
- -DBoost_ROOT=%DEPLIBS_DIR%\boost_1_84_0 ^
- -DLIBCUEMOL2_ROOT=%DEPLIBS_DIR%\cuemol2 ^
- -DCMAKE_BUILD_TYPE=%CONFIG% ^
+ -DCMAKE_PREFIX_PATH=%BASEDIR% ^
+ -DBoost_ROOT=%BASEDIR%\boost_%BOOST_VER% ^
+ -DLIBCUEMOL2_ROOT=%BASEDIR%\cuemol2 ^
+ -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
  %SCCACHE%
 
-cmake --build %BUILDDIR% --target clean --config %CONFIG%
-cmake --build %BUILDDIR% --parallel --config %CONFIG%
-cmake --install %BUILDDIR% --config %CONFIG%
+cmake --build %BUILD_DIR% --target clean --config %BUILD_TYPE%
+cmake --build %BUILD_DIR% --parallel --config %BUILD_TYPE%
+cmake --install %BUILD_DIR% --config %BUILD_TYPE%
 
-copy %DEPLIBS_DIR%\boost_1_84_0\lib\*mt-x64*.dll %INSTPATH%\bin\
+copy %BASEDIR%\boost_%BOOST_VER%\lib\*mt-x64*.dll %INSTPATH%\bin\
 
 REM check run
 %INSTPATH%\bin\cuetty.exe
