02baf3ead21f9cc215ff40f629b837cf1bbe0293
ishitani <ishitani@users.sourceforge.net>
2025-12-29 23:02:42 +0900

    fix numpy support of pymod

--- tests/qlib_tests/test_bytearray.py
+++ tests/qlib_tests/test_bytearray.py
@@ -35,3 +35,16 @@ def test_qlib_bytearray_getfsetf(ba_obj):
 #     # uint8 array of 100 elems
 #     ba_obj.init(1, 100)
 #     ba_obj.setValue(1000, 10)
+
+def test_numpy_array():
+    print(dir(cuemol.ci))
+    arr = cuemol.ci.numpychk()
+    print(arr)
+    assert len(arr) == 10
+    
+def test_numpy_array_ba(ba_obj):
+    ba_obj.init(21, 100)
+    # print(dir(ba_obj._wrapped))
+    arr = cuemol.ci.tondarray(ba_obj._wrapped)
+    print(arr)
+    
