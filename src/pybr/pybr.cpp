fbe5a5f761f6d7556156288b5401fa1a6f8b69e4
ishitani <ishitani@users.sourceforge.net>
2026-01-04 17:30:08 +0900

    .

--- src/pybr/pybr.cpp
+++ src/pybr/pybr.cpp
@@ -32,7 +32,7 @@ PyObject *isInitialized(PyObject *self, PyObject *args)
 {
     Py_RETURN_TRUE;
 }
-}
+}  // namespace
 
 static PyMethodDef module_methods[] = {
     {"initCueMol", (PyCFunction)initCueMol, METH_VARARGS,
@@ -40,20 +40,19 @@ static PyMethodDef module_methods[] = {
     {"finiCueMol", (PyCFunction)finiCueMol, METH_VARARGS, "finalize CueMol system.\n"},
     {"isInitialized", (PyCFunction)isInitialized, METH_VARARGS,
      "check initialization.\n"},
-    {NULL, NULL, 0, NULL}
-};
+    {NULL, NULL, 0, NULL}};
 
 namespace pybr {
 
- PyObject *initModuleFunc(void)
- {
-     auto m = Wrapper::init();
-     PyModule_AddFunctions(m, module_methods);
-     return m;
- }
+PyObject *initModuleFunc(void)
+{
+    auto m = Wrapper::init();
+    PyModule_AddFunctions(m, module_methods);
+    return m;
+}
 
-  bool init(const char *szConfPath)
-  {
+bool init(const char *szConfPath)
+{
     pybr_regClasses();
 
     Py_SetProgramName(Py_DecodeLocale("cuemol2", NULL));
@@ -63,79 +62,64 @@ namespace pybr {
 #ifdef HAVE_LOCAL_PYTHON
     // Case: GUI application with local python installation
     //   --> Set local python path as PYTHONHOME
-    if (szConfPath!=NULL) {
-      fs::path confpath(szConfPath);
-      confpath = confpath.parent_path();
-      confpath /= "Python";
-      if (fs::exists(confpath) && fs::is_directory(confpath)) {
-        LString strpath = confpath.string();
-        strpath = strpath.escapeQuots();
-        Py_SetPythonHome(Py_DecodeLocale(strpath.c_str(), NULL));
-	LOG_DPRINTLN("Python> SetPythonHome=%s", strpath.c_str());
-      }
+    if (szConfPath != NULL) {
+        fs::path confpath(szConfPath);
+        confpath = confpath.parent_path();
+        confpath /= "Python";
+        if (fs::exists(confpath) && fs::is_directory(confpath)) {
+            LString strpath = confpath.string();
+            strpath = strpath.escapeQuots();
+            Py_SetPythonHome(Py_DecodeLocale(strpath.c_str(), NULL));
+            LOG_DPRINTLN("Python> SetPythonHome=%s", strpath.c_str());
+        }
     }
 #endif
 
     Py_Initialize();
 
-    // hook import to import cuemol._internal
-    PyRun_SimpleString("import importlib.abc\n"
-		       "import importlib.machinery\n"
-		       "import sys\n"
-		       "class Finder(importlib.abc.MetaPathFinder):\n"
-		       "    def find_spec(self, fullname, path, target=None):\n"
-		       "        if fullname in sys.builtin_module_names:\n"
-		       "            return importlib.machinery.ModuleSpec(\n"
-		       "                fullname,\n"
-		       "                importlib.machinery.BuiltinImporter,\n"
-		       "            )\n"
-		       "sys.meta_path.append(Finder())\n");
-
-    //LOG_DPRINTLN("Python> PythonHome=%s", Py_EncodeLocale(Py_GetPythonHome(), NULL));
+    // LOG_DPRINTLN("Python> PythonHome=%s", Py_EncodeLocale(Py_GetPythonHome(), NULL));
 
     // Append local python script path to sys.path
-    if (szConfPath!=NULL) {
-      fs::path confpath(szConfPath);
-      confpath = confpath.parent_path();
-      confpath /= "python";
-      LString strpath = confpath.string();
-      strpath = strpath.escapeQuots();
-      
-      LString src = LString::format(
-        "import sys\n"
-        "sys.path.append('%s')\n",
-        //"print(sys.path)\n",
-        strpath.c_str());
+    if (szConfPath != NULL) {
+        fs::path confpath(szConfPath);
+        confpath = confpath.parent_path();
+        confpath /= "python";
+        LString strpath = confpath.string();
+        strpath = strpath.escapeQuots();
 
-      PyRun_SimpleString(src.c_str());
-      LOG_DPRINTLN("Python> local script path=%s added", strpath.c_str());
+        LString src = LString::format(
+            "import sys\n"
+            "sys.path.append('%s')\n",
+            //"print(sys.path)\n",
+            strpath.c_str());
+
+        PyRun_SimpleString(src.c_str());
+        LOG_DPRINTLN("Python> local script path=%s added", strpath.c_str());
     }
 
     // Redirect stdout/err to the logwindow
     PyRun_SimpleString(
-"import sys\n"
-//"import cuemol._internal as cuemol_internal\n"
-"import _cuemol_internal as ci\n"
-"class CatchOutErr:\n"
-"    def __init__(self):\n"
-"        self.value = ''\n"
-"    def write(self, txt):\n"
-"        ci.print(txt)\n"
-"    def flush(self):\n"
-"        pass\n"
-"catchOutErr = CatchOutErr()\n"
-"sys.stdout = catchOutErr\n"
-"sys.stderr = catchOutErr\n"
-);
+        "import sys\n"
+        "import _cuemol_internal as ci\n"
+        "class CatchOutErr:\n"
+        "    def __init__(self):\n"
+        "        self.value = ''\n"
+        "    def write(self, txt):\n"
+        "        ci.print(txt)\n"
+        "    def flush(self):\n"
+        "        pass\n"
+        "catchOutErr = CatchOutErr()\n"
+        "sys.stdout = catchOutErr\n"
+        "sys.stderr = catchOutErr\n");
 
     LOG_DPRINTLN("Python> initialize OK.");
     LOG_DPRINTLN("Python> %s.", Py_GetVersion());
 
     return true;
-  }
-
-  void fini()
-  {
-    Py_Finalize();
-  }
 }
+
+void fini()
+{
+    Py_Finalize();
+}
+}  // namespace pybr
