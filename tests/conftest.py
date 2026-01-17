2e54af1622a3bf42eba902ad52fcfa310d0760ec
ishitani <ishitani@users.sourceforge.net>
2026-01-17 11:04:06 +0900

    add selection all none pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
+import cuemol
 from pathlib import Path
+
 
 @pytest.fixture
 def test_data_path():
@@ -10,3 +12,17 @@ def test_data_path():
 @pytest.fixture
 def mol_1crn_path(test_data_path):
     return test_data_path / "1CRN.pdb"
+
+
+@pytest.fixture
+def create_scene():
+    mgr = cuemol.svc("CmdMgr")
+
+    args = {"scene_name": "new scene", "create_view": False}
+    result = mgr.runCmdArgs("new_scene", args)
+    scene = result["result_scene"]
+    yield scene
+
+    # clean-up
+    scmgr = cuemol.svc("SceneManager")
+    scmgr.destroyAllScenes()
