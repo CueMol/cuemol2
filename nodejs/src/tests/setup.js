1d478149bbaedfc637984a5ee9d7ba8cd14ccd6a
ishitani <ishitani@users.sourceforge.net>
2026-02-07 17:06:01 +0900

    add jest tests

---
+++ nodejs/src/tests/setup.js
@@ -0,0 +1,7 @@
+/**
+ * Shared CueMol instance for test suites.
+ * Import `cm` to create CueMol objects in tests.
+ */
+import * as core from '../index.js';
+
+export const cm = core.createCueMol();
