0408aadaec32b6ef50f06a6bcf1cf76f0b8459e1
ishitani <ishitani@users.sourceforge.net>
2026-03-14 15:25:38 +0900

    add autoconf213 install setp to gha workflow

--- build_scripts/build_uxpgui_posix/run.sh
+++ build_scripts/build_uxpgui_posix/run.sh
@@ -63,8 +63,6 @@ ls -l $BUNDLE_DIR/ffmpeg
 ls -l $BUNDLE_DIR/apbs
 
 if [ $RUNNER_OS = "macOS" ]; then
-    brew install autoconf@2.13 yasm
-
     SDK_BASE_PATH=$(dirname $(xcrun --show-sdk-path))
     SDK_PATH=$(echo $SDK_BASE_PATH/MacOSX*.*.sdk | tr ' ' '\n' | sort | tail -1)
     echo SDK_PATH: $SDK_PATH
